d8da9434fd976b14ad9b159505b843fd263a82c2
Yuichiro Take <y.takey@gmail.com>
2021-11-23 18:05:49 +0900

    Pagination on the client using fetchMore()

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,50 @@ import { gql, useQuery } from "@apollo/client";
 import { AwesomeLink } from "../components/AwesomeLink";
 
 const AllLinksQuery = gql`
-  query {
-    links {
-      id
-      title
-      url
-      description
-      imageUrl
-      category
+  query allLinksQuery($first: Int, $after: String) {
+    links(first: $first, after: $after) {
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+      edges {
+        cursor
+        node {
+          id
+          title
+          url
+          description
+          imageUrl
+          category
+        }
+      }
     }
   }
 `;
 
 export default function Home() {
-  const { data, loading, error } = useQuery(AllLinksQuery);
+  const { data, loading, error, fetchMore } = useQuery(AllLinksQuery, {
+    variables: { first: 2 },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Oh no...{error.message}</p>;
+
+  const { endCursor, hasNextPage } = data.links.pageInfo;
+
+  const handleMore = () => {
+    // TODO: typing correctly
+    fetchMore<{ links: { edges: any[] } }, { after: number }>({
+      variables: { after: endCursor },
+      updateQuery: (prevResult, { fetchMoreResult }) => {
+        fetchMoreResult.links.edges = [
+          ...prevResult.links.edges,
+          ...fetchMoreResult.links.edges,
+        ];
+        return fetchMoreResult;
+      },
+    });
+  };
 
   return (
     <div>
@@ -30,18 +57,30 @@ export default function Home() {
 
       <div className="container mx-auto max-w-5xl my-20">
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {data.links.map((link) => (
+          {data.links.edges.map(({ node }) => (
             <AwesomeLink
-              key={link.id}
-              url={link.url}
-              id={link.id}
-              category={link.category}
-              title={link.title}
-              description={link.description}
-              imageUrl={link.imageUrl}
+              key={node.id}
+              url={node.url}
+              id={node.id}
+              category={node.category}
+              title={node.title}
+              description={node.description}
+              imageUrl={node.imageUrl}
             />
           ))}
         </ul>
+        {hasNextPage ? (
+          <button
+            className="px-4 py-2 bg-blue-500 text-white rounded my-10"
+            onClick={handleMore}
+          >
+            more
+          </button>
+        ) : (
+          <p className="my-10 text-center font-medium">
+            You've reached the end!{" "}
+          </p>
+        )}
       </div>
     </div>
   );
